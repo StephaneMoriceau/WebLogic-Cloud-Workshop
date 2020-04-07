0341b1d4319dd1728fe0edb1cf0684fa72f182a4
StephaneMoriceau <stephane.moriceau@oracle.com>
2020-04-07 14:42:58 +0800

    use branch url instead of master

--- terraform/download.sh
+++ terraform/download.sh
@@ -1,4 +1,4 @@
-[ -d ~/WLSHOL-terraform ] && rm -r WLSHOL-terraform
+[ -d ~/WebLogic-Cloud-Workshop ] && rm -r WebLogic-Cloud-Workshop
 
 printf "\nCreating directory WLSHOL-terraform ...\n\n"
 
@@ -8,12 +8,12 @@ cd WLSHOL-terraform
 
 printf "Downloading terraform scripts in ~/WLSHOL-terraform ...\n\n"
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/main.tf --output main.tf
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/update-section%235/terraform/main.tf --output main.tf
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/provider.tf --output provider.tf
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/update-section%235/terraform/provider.tf --output provider.tf
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/terraform.tfvars.example --output terraform.tfvars.example
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/update-section%235/terraform/terraform.tfvars.example --output terraform.tfvars.example
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/variables.tf --output variables.tf
+https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/update-section%235/terraform/variables.tf --output variables.tf
 
 printf "\nDownload complete.\n\n"
