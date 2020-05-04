be9c33e84593145dd4283b8f3b644f8ccf210b41
StephaneMoriceau <stephane.moriceau@oracle.com>
2020-05-04 11:31:18 +0800

    updated to target main branch

--- terraform/download.sh
+++ terraform/download.sh
@@ -8,12 +8,12 @@ cd WebLogic-Cloud-Workshop
 
 printf "Downloading terraform scripts in ~/WebLogic-Cloud-Workshop ...\n\n"
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/update-section%235/terraform/main.tf --output main.tf
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/main/terraform/main.tf --output main.tf
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/update-section%235/terraform/provider.tf --output provider.tf
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/main/terraform/provider.tf --output provider.tf
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/update-section%235/terraform/terraform.tfvars.example --output terraform.tfvars.example
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/main/terraform/terraform.tfvars.example --output terraform.tfvars.example
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/update-section%235/terraform/variables.tf --output variables.tf
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/main/terraform/variables.tf --output variables.tf
 
 printf "\nDownload complete.\n\n"
