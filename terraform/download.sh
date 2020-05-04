df8c653d1abfe39d42cac5c9f47e5c0ad50d20b7
StephaneMoriceau <stephane.moriceau@oracle.com>
2020-05-04 11:33:05 +0800

    updated to target master branch

--- terraform/download.sh
+++ terraform/download.sh
@@ -8,12 +8,12 @@ cd WebLogic-Cloud-Workshop
 
 printf "Downloading terraform scripts in ~/WebLogic-Cloud-Workshop ...\n\n"
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/main/terraform/main.tf --output main.tf
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/main.tf --output main.tf
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/main/terraform/provider.tf --output provider.tf
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/provider.tf --output provider.tf
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/main/terraform/terraform.tfvars.example --output terraform.tfvars.example
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/terraform.tfvars.example --output terraform.tfvars.example
 
-curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/main/terraform/variables.tf --output variables.tf
+curl https://raw.githubusercontent.com/StephaneMoriceau/WebLogic-Cloud-Workshop/master/terraform/variables.tf --output variables.tf
 
 printf "\nDownload complete.\n\n"
